cde7ee0a3008379146898f4125674fdf01e8da86
Aaron Adrian <aaroncadrian@gmail.com>
2022-05-18 00:50:24 -0700

    Add lambda

--- infra/lambda_http_handler.tf
+++ infra/lambda_http_handler.tf
@@ -1,5 +1,28 @@
 #region IAM Policy
 
+resource "aws_iam_role" "lambda_exec" {
+  name = "${var.app_name}.${var.environment_name}.lambda-role"
+
+  assume_role_policy = jsonencode({
+    Version   = "2012-10-17",
+    Statement = [
+      {
+        Action    = "sts:AssumeRole"
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+        Effect    = "Allow"
+        Sid       = ""
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_policy" {
+  role       = aws_iam_role.lambda_exec.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
 #resource "aws_iam_role_policy" "lambda_policy" {
 #  name = "${var.app_name}.${var.environment_name}.lambda-policy"
 #
@@ -21,11 +44,7 @@
 #  })
 #}
 
-data "archive_file" "http_handler" {
-  output_path = "${path.module}/http_handler.zip"
-  source_dir  = "${path.module}/../WebApplication1/bin/Debug/net6.0"
-  type        = "zip"
-}
+
 #
 #data "aws_iam_policy_document" "http_handler" {
 #  version = "2012-10-17"
@@ -47,13 +66,38 @@ data "archive_file" "http_handler" {
 #  assume_role_policy = ""
 #}
 #
-##endregion
-#
-##region Lambda Function
+#endregion
+
+#region Lambda Function
+
+data "archive_file" "http_handler" {
+  output_path = "${path.module}/http_handler.zip"
+  source_dir  = "${path.module}/../WebApplication1/bin/Debug/net6.0"
+  type        = "zip"
+}
+
+resource "aws_lambda_function" "svc_function" {
+  function_name = "${var.app_name}_${var.environment_name}"
+
+  handler          = "main.handler"
+  filename         = data.archive_file.http_handler.output_path
+  source_code_hash = data.archive_file.http_handler.output_base64sha256
+
+  role = aws_iam_role.lambda_exec.arn
+
+  runtime = "dotnet6"
+
+#  environment {
+#    variables = {
+#      DYNAMO_TABLE_NAME = aws_dynamodb_table.primary_table.name
+#    }
+#  }
+}
+
 #
 #resource "aws_lambda_function" "http_handler" {
 #  function_name = ""
 #  role          = aws_iam_role.http_handler.arn
 #}
 #
-##endregion
+#endregion
--- infra/variables.tf
+++ infra/variables.tf
@@ -2,3 +2,13 @@ variable "aws_region" {
   type    = string
   default = "us-west-2"
 }
+
+variable "app_name" {
+  type    = string
+  default = "z-aaron-weather-forecast"
+}
+
+variable "environment_name" {
+  type    = string
+  default = "poc"
+}
