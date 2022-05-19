21e11ddeb69e4680b5f56b5bca7995d5e9a3ec33
Aaron Adrian <aaroncadrian@gmail.com>
2022-05-18 17:20:25 -0700

    Add environment variables

--- WebApplication1/Program.cs
+++ WebApplication1/Program.cs
@@ -4,6 +4,8 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Configuration.AddEnvironmentVariables();
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
