88891280e746c6e3d1d0e9fdfe13099ead6c8e0c
Aaron Adrian <aaroncadrian@gmail.com>
2022-05-18 17:17:14 -0700

    Add seed data to dynamo

--- infra/01-dynamodb.tf
+++ infra/01-dynamodb.tf
@@ -20,3 +20,28 @@ resource "aws_dynamodb_table" "primary_table" {
 }
 
 #endregion
+
+#region DynamoDB Seed Data
+
+resource "aws_dynamodb_table_item" "seed_data" {
+  table_name = aws_dynamodb_table.primary_table.name
+
+  hash_key  = "pk"
+  range_key = "sk"
+  
+  item = jsonencode({
+    pk = {
+      S = "PEOPLE"
+    }
+
+    sk = {
+      S = "1"
+    }
+
+    firstName = {
+      S = "DEMO_FIRST_NAME"
+    }
+  })
+}
+
+#endregion
